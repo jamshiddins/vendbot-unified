af935243c7222f16b3336cd975b3b051c801f34b
jamshiddins <jamshiddin@gmail.com>
2025-06-21 22:26:32 +0500

    Add users router to handlers initialization

--- backend/bot/handlers/__init__.py
+++ backend/bot/handlers/__init__.py
@@ -1,20 +1,23 @@
 ﻿from aiogram import Router
 
-# Импортируем все роутеры
 from .start import router as start_router
+from .admin import router as admin_router
+from .warehouse import router as warehouse_router
+from .operator import router as operator_router
+from .hopper_management import router as hopper_router
+from .admin_machines import router as admin_machines_router
+from .owner import router as owner_router
 from .roles import router as roles_router
-from .users import router as users_router  
-from .machines import router as machines_router
-from .hoppers import router as hoppers_router
+from .users import router as users_router  # Добавляем импорт
 
-# Главный роутер
-router = Router()
-
-# Подключаем все роутеры
-router.include_router(start_router)
-router.include_router(roles_router)  # Важно: roles идет перед users
-router.include_router(users_router)
-router.include_router(machines_router)
-router.include_router(hoppers_router)
-
-__all__ = ["router"]
+def setup_handlers(dp):
+    """Регистрация всех хендлеров"""
+    dp.include_router(start_router)
+    dp.include_router(users_router)  # Добавляем регистрацию
+    dp.include_router(admin_router)
+    dp.include_router(warehouse_router)
+    dp.include_router(operator_router)
+    dp.include_router(hopper_router)
+    dp.include_router(admin_machines_router)
+    dp.include_router(owner_router)
+    dp.include_router(roles_router)
